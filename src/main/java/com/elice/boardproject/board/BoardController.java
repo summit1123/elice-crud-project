de23cf6b94ebfd12b952c8b03675bec99da954dd
김동현 <kimdonghyeon@kimdonghyeonui-MacBookAir.local>
2024-04-25 00:46:06 +0900

    엔티티,컨트롤러,레퍼지토리 생성

---
+++ src/main/java/com/elice/boardproject/board/BoardController.java
@@ -0,0 +1,32 @@
+package com.elice.boardproject.board;
+
+import com.elice.boardproject.board.entity.Board;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BoardController {
+    @RequestMapping(value = "/")
+    public ModelAndView first() {
+        List<Board> boards = new ArrayList<>();
+        Board board = new Board(1,"김동현", "love you");
+        boards.add(board);
+        Board board1 = new Board(2,"장민성", "hate you");
+        boards.add(board1);
+        ModelAndView Mav = new ModelAndView("board/boards");
+        Mav.addObject("boards", boards);
+        return Mav;
+
+    }
+
+    @RequestMapping(value = "/boards/create")
+    public ModelAndView second() {
+        ModelAndView Mav = new ModelAndView("board/createBoard");
+        return Mav;
+    }
+}
