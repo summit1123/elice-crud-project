00f582f1729171a15c9fdd373093836ac566a066
김동현 <kimdonghyeon@kimdonghyeonui-MacBookAir.local>
2024-04-25 15:05:28 +0900

    됐는 확인

--- src/main/java/com/elice/boardproject/board/BoardController.java
+++ src/main/java/com/elice/boardproject/board/BoardController.java
@@ -21,8 +21,8 @@ public class BoardController {
         ModelAndView Mav = new ModelAndView("board/boards");
         Mav.addObject("boards", boards);
         return Mav;
-포ㅗㅗㅗㅗㅎ롷ㅗㅎㅗ
-    }
+
+    }ddddd
 
     @RequestMapping(value = "/boards/create")
     public ModelAndView second() {
