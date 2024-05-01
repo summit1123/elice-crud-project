13a45d9d59509df86aa38ade54a7e4486faafa6a
김동현 <ruggy245@naver.com>
2024-05-01 18:38:29 +0900

    feat: postService(예외처리)

--- src/main/java/com/elice/boardproject/service/PostService.java
+++ src/main/java/com/elice/boardproject/service/PostService.java
@@ -1,9 +1,11 @@
 package com.elice.boardproject.service;
+import com.elice.boardproject.PostNotFoundException;
 import com.elice.boardproject.entity.Board;
 import com.elice.boardproject.entity.Post;
 import com.elice.boardproject.repository.BoardRepository;
 import com.elice.boardproject.repository.PostRepository;
 import jakarta.transaction.Transactional;
+import java.sql.Timestamp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,7 +20,7 @@ public class PostService {
 
     public Post getPostById(int postId) {
         return postRepository.findById(postId)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid post id: " + postId));
+            .orElseThrow(() -> new PostNotFoundException("Post not found with id: " + postId));
     }
 
     public Page<Post> getPostsByBoard(Board board, Pageable pageable) {
@@ -30,7 +32,9 @@ public class PostService {
         Board board = boardRepository.findById(boardId)
             .orElseThrow(() -> new IllegalArgumentException("Invalid board id: " + boardId));
         post.setBoard(board);
+        post.setCreated_at(new Timestamp(System.currentTimeMillis()));
         return postRepository.save(post);
+
     }
 
     @Transactional
